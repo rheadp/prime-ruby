3c3aab51dd97871a5201fe9de639e077efe3c752
rheadp <rheadpea@msu.edu>
2021-02-15 11:23:50 -0500

    commit tree 8e74284110b5cd6a52a747c7f365b3ac931c4b02

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+    first = 2 
+    if num > 1
+        range = (first..num-1).to_a
+        range.none? do |i|
+            num % i == 0
+        end
+    else
+        false
+    end
+end
